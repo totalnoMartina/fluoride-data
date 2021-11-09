6f50f3be1c56a2ddd16a421f1dd5012aa101471b
Martina Grganic <totalnobzvz@yahoo.com>
2021-11-09 15:27:05 +0000

    Add inital commit and first test of python code

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+
+def welcome_user():
+    ask_usr = input('What is your name? \n')
+    name = ask_usr
+    msg = print(f'Welcome to Exploration of Data on Fluoride ${name}')
+    return msg
