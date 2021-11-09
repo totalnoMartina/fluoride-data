13ceebb0c3db8d17385ec94395dd30717bd95741
Martina Grganic <totalnobzvz@yahoo.com>
2021-11-09 16:37:46 +0000

    Add lxml package

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 # Your code goes here.
 # You can delete these comments, but do not change the name of this file
 # Write your code to expect a terminal of 80 characters wide and 24 rows high
+import lxml
+
+
 
 def welcome_user():
     ask_usr = input('What is your name? \n')
